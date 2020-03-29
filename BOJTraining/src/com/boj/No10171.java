2a4e0a3e869b2ac5e24b18cd1f89440fae43bc69
devGamja <devPotato777@gmail.com>
2020-03-30 00:57:11 +0900

    No10171 고양이 성공

---
+++ BOJTraining/src/com/boj/No10171.java
@@ -0,0 +1,26 @@
+package com.boj;
+
+/**
+ * 
+ * @author POTATO 
+ * 		   [문제]
+ * 		   아래 예제와 같이 고양이를 출력하시오.
+ * 
+ *         [입력]
+ *         없음.
+ * 
+ *         [출력]
+ *         고양이를 출력한다.
+ *
+ */
+
+public class No10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
