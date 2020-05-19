9394dba51c358b55714c199c4cdfa917310d4433
devGamja <devPotato777@gmail.com>
2020-05-20 00:00:03 +0900

    -

--- BOJTraining/src/com/boj/level5/No2446.java
+++ BOJTraining/src/com/boj/level5/No2446.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 /**
  * 
  * @author POTATO 
- *            [문제] 별 찍기 - 9
- *            예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+ *         [문제] 별 찍기 - 9
+ *         예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
  * 
  *         [입력] 
  *         첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
@@ -16,35 +16,35 @@ import java.util.Scanner;
  *
  */
 
-public class No2523 {
+public class No2446 {
 
-    public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
-        
-        for(int p = 0; p<n; p++) {
-            for(int q=0; q<p;q++) {
-                System.out.print(" ");
-            }
-            for(int q=0; q<2*(n-p)-1;q++) {
-                System.out.print("*");
-            }
- 
-            System.out.println("");
-        }
-        
-        for(int p = 1; p<n; p++) {
-            for(int q=0; q<n-p-1;q++) {
-                System.out.print(" ");
-            }
-            for(int q=0; q<2*p+1;q++) {
-                System.out.print("*");
-            }
- 
-            System.out.println("");
-        }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
 
-    }
+		for (int p = 0; p < n; p++) {
+			for (int q = 0; q < p; q++) {
+				System.out.print(" ");
+			}
+			for (int q = 0; q < 2 * (n - p) - 1; q++) {
+				System.out.print("*");
+			}
+
+			System.out.println("");
+		}
+
+		for (int p = 1; p < n; p++) {
+			for (int q = 0; q < n - p - 1; q++) {
+				System.out.print(" ");
+			}
+			for (int q = 0; q < 2 * p + 1; q++) {
+				System.out.print("*");
+			}
+
+			System.out.println("");
+		}
+
+	}
 
 }
