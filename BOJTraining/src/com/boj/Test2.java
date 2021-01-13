446444dfcf5b159b747bf826b46ad26ced67ad42
devPotato777 <devPotato777@gmail.com>
2021-01-14 04:08:46 +0900

    update

--- BOJTraining/src/com/boj/Test2.java
+++ BOJTraining/src/com/boj/Test2.java
@@ -1,9 +1,22 @@
 package com.boj;
 
+import java.util.Scanner;
+
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("wow");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		int g = sc.nextInt();
+		
+		String s = ""+a+b+c+d+e+f+g;
+		System.out.println(s);
+		
 
 	}
 
